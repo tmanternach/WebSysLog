bdae9b4be5af16553aa11118d2f7ef1ae9ed282e
Ken <ken.joss1@gmail.com>
2017-07-10 15:10:07 +0200

    v2.0

--- distinct.php
+++ distinct.php
@@ -1,14 +1,15 @@
 <?php
-require_once("config.php");
-$sql="SELECT DISTINCT FromHost FROM SystemEvents ORDER BY FromHost DESC";
-$result = mysql_query($sql);
-$arr = array();
-while($row = mysql_fetch_array($result))
-{
-	//if (strpos($row['FromHost'],$_GET['term']) !== false) {
-		$arr[] = $row['FromHost'];
-	//}
-}
-echo json_encode($arr);
-mysql_close($con);
+	require_once("config.php");
+	$sql="SELECT DISTINCT host FROM syslog.syslog ORDER BY host DESC";
+	$result = mysql_query($sql);
+	$arr = array();
+	
+	while($row = mysql_fetch_array($result)) {
+		//if (strpos($row['FromHost'],$_GET['term']) !== false) {
+			$arr[] = $row['host'];
+		//}
+	}
+	
+	echo json_encode($arr);
+	mysql_close($con);
 ?>
